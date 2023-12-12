36ac8c547a9eb201ee9b778d4d1dd8be6f028c33
Michaela <michaela.arteberry@gmail.com>
2023-12-12 16:27:57 -0500

    Working on exercise for syntax, types and variables

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,25 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+        //Starting a new exercise
+        int myFavoriteNumber = 7;
+        System.out.println(myFavoriteNumber);
+        String myString = "Ill never give up! That's my ninja way!";
+        System.out.println(myString);
+        long myNumber = 123;
+        System.out.println(myNumber);
+        float myNumber2 = myNumber;
+        System.out.println(myNumber2);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//First block of code prints  out 5 then 6
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+        //Second block of code prints out 6 then 6
+        int x = (4+5);
+        System.out.println(x);
     }
 }
